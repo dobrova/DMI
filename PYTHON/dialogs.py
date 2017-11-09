4272c7b14464897eee30e34904be0f5b23b8269d
user <ksenija288@inbox.lv>
2017-11-09 09:54:05 +0200

    20171109_09_54

---
+++ PYTHON/dialogs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+a = input("lietotaj, lūdzu ievadi kaut ko: ")
+print "tu ievadiji lielumu ar %s datu tipu"%(type(a))
+print a * a
+print a + a
+
+	
+a = raw_input("lietotaj, lūdzu ievadi kaut ko: ")
+print "tu ievadiji lielumu ar %s datu tipu"%(type(a))
+#print a * a
+print a + a
+'''
+
+
+a = raw_input("lietotaj lūdzu ievadi savu vārdu:")
+b = raw_input("lietotaj lūdzu ievadi savu uzvārdu:")
+print "Tatad tevi sauc - %s"%(a + ' ' + b)
+print "Tatad tevi sauc - %s"%(a + chr(32) + b)
+
