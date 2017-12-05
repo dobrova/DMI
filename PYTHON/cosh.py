0fb0fa08feddf1bf1e8419677647a477fa26d048
user <ksenija288@inbox.lv>
2017-12-05 05:16:08 -0500

    20171205_05_16

--- PYTHON/cosh.py
+++ PYTHON/cosh.py
@@ -8,10 +8,11 @@ def mans_cos(x,n):
     k=0
     S=0
     while k <= n:
-        k += 1
         a = x**(2*k)/math.factorial(2*k)
         S += a
+        k += 1
     return S
+
 x = np.arange (0.0,4,0.01)
 y = np.cosh(x)
 
@@ -21,7 +22,7 @@ colors=['r','g','b','y','c','k']
 for i in range(0,5,1):
      f = mans_cos(x,i)
      plt.plot(x,f, colors[i])
-  
+
 plt.show ()
 
 
