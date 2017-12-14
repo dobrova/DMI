2b782a477ae68c0d81aca928ef25404598d2fb2f
user <ksenija288@inbox.lv>
2017-12-14 11:59:44 +0200

    20171214_11_59

---
+++ PYTHON/tabl.py
@@ -0,0 +1,9 @@
+skaitlis=input("Ievadiet skaitli: ")
+print "\nsk\t%1\t%2\t%3\t%4\t%5\n"
+
+for x in range(skaitlis, skaitlis+11, 1):
+	print "%d \t"%(x),
+	for i in range(1, 6, 1):
+		sk_mod=x%i
+		print "%d \t"%(sk_mod),
+print "\n" 
