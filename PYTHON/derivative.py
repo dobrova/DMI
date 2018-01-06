808150cca77424d9886d7623d84cede92bc191e1
user <ksenija288@inbox.lv>
2018-01-06 15:21:53 +0200

    20180106_15_21

--- PYTHON/derivative.py
+++ PYTHON/derivative.py
@@ -4,28 +4,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def mans_sinuss (x):
-     
-
-     k = 0
-     a = (-1)**0*x**1/(1)
-     S = a
-
-     
-     while k<500:
-          k = k + 1
-          R = (-1) * x**2 /(2*k*(2*k+1))
-          a = a * R
-          S = S + a
+def mans_cos(x):
+     k=0
+     S=0
+     while k <= 70:
+          a = x**(2*k)/np.math.factorial(2*k)
+          S += a
+          k += 1
      return S
+
 a = 0
 b = 3 * np.pi
 x = np.arange(a,b,0.5)
-y = mans_sinuss(x)
+y = mans_cos(x)
 
 plt.plot(x,y)
 plt.grid()
-#plt.show()
+
 
 
 
@@ -34,18 +29,9 @@ n = len(x)
 y_prim = []
 
 for i in range(n-1):
-     #print i, x[i], y[i] ,
      delta_y = y[i+1] - y[i]
      delta_x = x[i+1] - x[i]
-     #y_prim = delta_y / delta_x
-     #print y_prim
      y_prim.append(delta_y / delta_x)
-
-#plt.plot (x[:n-1],y_prim)
-#plt.show()
-
-
-
 
 
 n = len(x)
@@ -60,32 +46,3 @@ for i in range(n-2):
 plt.plot (x[:n-1],y_prim)
 plt.plot (x[:n-2],y_2prim)
 plt.show()
-
-
-
-
-
-
-
-
-'''
-k = k + 1
-a = a * (-1) * x**2 /(2*k(2*k+1))
-S = S + a
-print "a1 = %6.2f S1 = %6.2f"%(a,S)
-
-
-
-k = k + 1
-a = a * (-1) * x**2 /(2*k(2*k+1))
-S = S + a
-print "a2 = %6.2f S2 = %6.2f"%(a,S)
-
-
-
-
-k = k + 1
-a = a * (-1) * x**2 /(2*k(2*k+1))
-S = S + a
-print "a3 = %6.2f S3 = %6.2f"%(a,S)
-'''
